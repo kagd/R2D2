e8098760b491e4b30c577bcee0d64226650381dd
Grant Klinsing <grant@digitalopera.com>
2015-03-05 08:31:40 -0700

    remove respond_to since unused

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  respond_to :json
   after_filter :cors_set_access_control_headers
 
   def cors
