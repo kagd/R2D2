0bb154b51d8b303bfaee69cee624d4c41a63a2f9
Grant Klinsing <grant@digitalopera.com>
2015-03-05 20:37:19 -0700

    Get the correct repo

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,6 +1,6 @@
 class GithubController < ApplicationController
   def index
-    @site_commits = Commit.where(repo_full_name: 'kagd/prototypeof').order('date DESC').group_by{|commit| commit.date.strftime('%B').downcase }
+    @site_commits = Commit.where(repo_full_name: ['kagd/R2D2', 'kagd/C3PO']).order('date DESC').group_by{|commit| commit.date.strftime('%B').downcase }
     @site_commits.keys.each do |month|
       @site_commits[month] = CommitPresenter.wrap @site_commits[month]
     end
